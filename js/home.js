275539443e2e13f1cc2d41aca9060da07c2e4d1c
Imamasharmeen <imamasharmeen@gmail.com>
2024-09-20 14:39:23 +0300

    Add money to the balance and verify pin Number

--- js/home.js
+++ js/home.js
@@ -3,7 +3,23 @@ document.getElementById('add-money-btn')
         event.preventDefault();
         
         const  addMoneyInput = document.getElementById('amount-money').value;
-        console.log(addMoneyInput);
+        //console.log(addMoneyInput);
         const  pinCodeInput = document.getElementById('pin-code-input').value;
-        console.log(pinCodeInput);
+        //console.log(pinCodeInput);
+        if(pinCodeInput === '1234'){
+            console.log('Adding money to your account.');
+            const balance = document.getElementById('account-balance').innerText
+            console.log(balance)
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber+balanceNumber;
+            console.log(newBalance);
+
+            document.getElementById('account-balance').innerText = newBalance
+        }
+
+           
+        else(
+            alert('Failed to adding money!Please try again')
+        )
 })
