de044fc811caa18d292b88cd44de3a2f32595ab7
Imamasharmeen <imamasharmeen@gmail.com>
2024-09-20 11:38:32 +0300

    Added cashout and add money

--- js/login.js
+++ js/login.js
@@ -7,7 +7,18 @@ document.getElementById('login-btn')
        // step - 3 get the phone number
        const phoneNumber = document.getElementById('phone-number').value;
        console.log(phoneNumber);
-       // step - 3 get the pin
+       // step - 4 get the pin
        const pinCode = document.getElementById('pin-code').value;
        console.log(pinCode);
+
+        // step-5: validate phone and Pin
+        // this is temporary. You should do like this.
+
+         if(phoneNumber=== '5' && pinCode === '1234'){
+             console.log('you are logged in');
+             // step -6: allow user to use the website
+         }
+         else{
+             alert('Wrong phone number or Pin');
+         }
 })
